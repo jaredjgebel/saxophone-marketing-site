fb0d80096f63d4954f8fcf0c50db175df24d1d37
Jared <jaredjgebel@gmail.com>
2021-03-18 10:47:42 -0400

    Add fonts and font variables

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,20 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>The Saxophone Store</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Righteous&family=Sora:wght@200;400&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <header className={styles.header}>
-        <div className={styles.headerText}></div>
+        <nav className={styles.headerNav}>
+          <a href="#">Location & Hours</a>
+          <div>The Saxophone Store</div>
+        </nav>
       </header>
 
       <main className={styles.main}></main>
